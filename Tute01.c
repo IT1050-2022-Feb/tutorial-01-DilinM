8080c0d723eeb97e798f2b1cd178a23408a7df7a
DilinM <99389027+DilinM@users.noreply.github.com>
2022-02-10 13:19:10 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,18 @@
 #include <stdio.h>
 
 int main() {
-  
+   int m1, m2;
+   float avg;
+   
+   printf("Enter Mark 1 : ");
+   scanf("%d", &m1);
+   
+   printf("Enter Mark 2 : ");
+   scanf("%d", &m2);
+   
+   avg = (m1+m2)/2;
+   
+   printf("Average = "%.2f", avg);
   return 0;
 }
 
